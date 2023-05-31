d4da29f5efd661eebff5f8178847a797983971e5
Noah Ripert <noah.ripert@edu.univ-fcomte.fr>
2023-05-31 23:08:18 +0200

    hero vue

---
+++ src/components/hero.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <div>
+        <h1>Titre de la page</h1>
+        <h2>Sous-titre de la page</h2>
+      </div>
+  
+      <div>
+        Texte constituant le chapo de la page.
+      </div>
+          <img src="../../img/orchestre.webp" alt="violons" />
+    </div>
+  </template>
---
+++ src/pages/events/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+	import Hero from '@/components/hero.vue'
+</script>
+<template>
+  <h1 class="gap-12">Page d'accueil</h1>
+  <Hero />
+</template>
