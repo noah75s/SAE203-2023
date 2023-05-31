341ac903e17a1c7283446afed07747aa42d0277e
Noah Ripert <noah.ripert@edu.univ-fcomte.fr>
2023-05-31 11:53:10 +0200

    footer

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -1,3 +1,43 @@
 <template>
-  Template pour le pied de page
+  <footer>
+    <nav>
+      <h4>Menu</h4>
+      <ul>
+        <li><a href="#">Mentions légales</a></li>
+        <li><a href="#">Projet réalisé dans le cadre d’un exercice pédagogique au département MMI de Montbéliard</a></li>
+        <li><a href="#">Contact et accès</a></li>
+        <li><a href="#">Noah Ripert C1</a></li>
+        <li><a href="#">ripertnoah@gmail.com</a></li>
+      </ul>
+    </nav>
+
+    <div>
+      <h4>Contact</h4>
+      <address>
+        Fondation Cartier, 261 Bd Raspail<br />75014 Paris<br />
+        <a href="tel:0384542727">03 84 54 27 27</a><br />
+        <a href="mailto:conservatoire@agglo-belfort.fr">conservatoire@agglo-belfort.fr</a>
+      </address>
+    </div>
+
+    <div></div>
+
+    <div>
+      <h4>Nous suivre</h4>
+      <ul>
+        <li>
+          <a href="https://facebook.com" aria-label="facebook"> </a>
+        </li>
+        <li>
+          <a href="https://instagram.com" aria-label="instagram"> </a>
+        </li>
+        <li>
+          <a href="https://twitter.com" aria-label="twitter"> </a>
+        </li>
+        <li>
+          <a href="https://youtube.com" aria-label="youtube"> </a>
+        </li>
+      </ul>
+    </div>
+  </footer>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -22,27 +22,11 @@ export default {
       transparent: 'transparent',
       gray: colors.zinc
     },
-    extend: {}
-  },
-  plugins: []
-}
-
-module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    colors: {
-      indigo: '#1d096b',
-      pink: '#feaa9b',
-      white: '#ffffff',
-      black: '#000000',
-      transparent: 'transparent',
-      gray: colors.zinc
-    },
-    fontFamily: {
-      sans: ['Mulish', 'sans-serif'],
-      serif: ['Space Mono', 'monospace']
-    },
-    extend: {}
+    extend: {
+      gridTemplateColumns: {
+        principal: 'repeat(12, minmax(0, 64px))'
+      }
+    }
   },
   plugins: []
 }
