9e08221ba0f56e1db692eba1b374a86fde7a86d5
Noah Ripert <noah.ripert@edu.univ-fcomte.fr>
2023-05-31 11:09:47 +0200

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,32 +7,46 @@ module.exports = {
   plugins: []
 }
 
+/** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+		colors: {
+      indigo: '#1d096b',
+      pink: '#feaa9b',
+      white: '#ffffff',
+      black: '#000000',
+      transparent: 'transparent',
+      gray: colors.zinc
+    },
+    extend: {}
+  },
+  plugins: []
+}
+
+/** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     colors: {
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
+      indigo: '#1d096b',
+      pink: '#feaa9b',
+      white: '#ffffff',
+      black: '#000000',
+      transparent: 'transparent',
+      gray: colors.zinc
     },
     fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
+      sans: ['Mulish', 'sans-serif'],
+      serif: ['Space Mono', 'monospace']
     },
-    extend: {
-      spacing: {
-        '8xl': '96rem',
-        '9xl': '128rem',
-      },
-      borderRadius: {
-        '4xl': '2rem',
-      }
-    }
+    extend: {}
   },
+  plugins: []
 }
