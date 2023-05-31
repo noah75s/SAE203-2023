4898b5ac16321df4c7d3befe034b5d884795bd59
Noah Ripert <noah.ripert@edu.univ-fcomte.fr>
2023-05-31 13:50:16 +0200

    header

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,3 +1,37 @@
+<script setup lang="ts">
+import LogoIcon from '@/components/icons/IconLogo.vue'
+import SearchIcon from '@/components/icons/IconSearch.vue'
+import { RouterLink } from 'vue-router'
+</script>
+
 <template>
-  Template pour l'en-tête de page
+  <header>
+    <div>
+      <a href="/">
+        <LogoIcon />
+      </a>
+    </div>
+
+    <div>
+      <button>
+        <SearchIcon />
+      </button>
+
+      <button>
+        <div></div>
+        <div></div>
+        <div></div>
+      </button>
+
+			<nav>
+		    <ul>
+		      <li><RouterLink to="/conservatoire">Infos sur l'exposition</RouterLink></li>
+		      <li><RouterLink to="/">Artistes</RouterLink></li>
+		      <li><RouterLink to="/events">Frise</RouterLink></li>
+		      <li><RouterLink to="/contact">Contact</RouterLink></li>
+		      <li><RouterLink to="/">Billetterie</RouterLink></li>
+		    </ul>
+		  </nav>
+    </div>
+  </header>
 </template>
