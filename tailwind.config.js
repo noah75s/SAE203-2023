bcedc821f4aaed4b10130e44412af08a978a0268
Noah Ripert <noah.ripert@edu.univ-fcomte.fr>
2023-05-31 11:12:53 +0200

    style css

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,10 +27,6 @@ export default {
   plugins: []
 }
 
-/** @type {import('tailwindcss').Config} */
-
-const colors = require('tailwindcss/colors')
-
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
