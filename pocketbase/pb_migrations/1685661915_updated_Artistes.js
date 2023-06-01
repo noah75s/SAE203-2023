c81228bc1d1c3f94ca3e5d3a2d6936bd9b2ad15d
Noah Ripert <noah.ripert@edu.univ-fcomte.fr>
2023-06-02 01:28:53 +0200

    rules

---
+++ pocketbase/pb_migrations/1685661915_updated_Artistes.js
@@ -0,0 +1,23 @@
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("k2j8irdn2uuzqec")
+
+  collection.listRule = ""
+  collection.viewRule = ""
+  collection.createRule = ""
+  collection.updateRule = ""
+  collection.deleteRule = ""
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("k2j8irdn2uuzqec")
+
+  collection.listRule = null
+  collection.viewRule = null
+  collection.createRule = null
+  collection.updateRule = null
+  collection.deleteRule = null
+
+  return dao.saveCollection(collection)
+})
